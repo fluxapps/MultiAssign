67ba0e8505c7a9eae05f5bb4407851cad1d21061
Fabian Schmid <fs@studer-raimann.ch>
2014-11-05 14:52:28 +0100

    Security Fix: Dont allow selecting own user account

--- classes/User/class.multaUserGUI.php
+++ classes/User/class.multaUserGUI.php
@@ -2,6 +2,7 @@
 require_once('class.multaUser.php');
 require_once('class.multaUserTableGUI.php');
 require_once('./Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/MultiAssign/classes/class.multaAccess.php');
+
 /**
  * Class multaUserGUI
  *
@@ -37,7 +38,7 @@ class multaUserGUI {
 
 
 	public function executeCommand() {
-		if(!multaAccess::hasAccess()) {
+		if (!multaAccess::hasAccess()) {
 			return false;
 		}
 		$cmd = $this->ilCtrl->getCmd(self::CMD_INDEX);
@@ -75,7 +76,12 @@ class multaUserGUI {
 
 
 	protected function selectUser() {
+		global $ilUser;
 		$usr_id = $_POST['id'];
+		if ($usr_id == $ilUser->getId()) {
+			ilUtil::sendFailure($this->pl->txt('msg_failure_own_usr_id'), true);
+			$this->ilCtrl->redirect($this, self::CMD_INDEX);
+		}
 		ilSession::set(self::SESSION_ID, $usr_id);
 		$this->ilCtrl->redirectByClass(array( 'multaCourseGUI' ));
 	}
