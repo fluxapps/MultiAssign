b7969c92f86a991d025a5b2f3c13cc47b05b8734
Martin Studer <ms@studer-raimann.ch>
2022-08-30 01:34:51 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'multa';
 $version = '2.2.0';
-$ilias_min_version = '5.4.0';
-$ilias_max_version = '6.999';
-$responsible = 'Fabian Schmid';
-$responsible_mail = 'support@studer-raimann.ch';
+$ilias_min_version = '6.0';
+$ilias_max_version = '7.999';
+$responsible = 'Martin Studer';
+$responsible_mail = 'support@fluxlabs.ch';
